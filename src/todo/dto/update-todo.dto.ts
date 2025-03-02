ad473c43defb6d090320f3924e008aca049594fb
Yuii24 <linian2040@gmail.com>
2025-03-02 17:05:11 +0800

    add dockerfile

--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,11 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
 import { Status } from './create-todo.dto';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateTodoDto extends PartialType(CreateTodoDto) {
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   event: string;
 
   @IsOptional()
